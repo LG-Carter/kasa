a7be46de8393f371db9f45958afc66def14da58c
LG-Carter <guillaume.leopoldie@gmail.com>
2025-11-19 16:14:54 +0100

    ajout des collapse sans css

---
+++ src/components/collapse/collapse.jsx
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+import cheveron from '../../assets/cheveron.png';
+
+function Collapse({title, children}) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleCollapse = () => {
+        setIsOpen(!isOpen);
+    }
+
+
+    return (
+        <div className='collapse'>
+            <div className='collapse_button' onClick={toggleCollapse}>
+                {title}
+                <img src={cheveron} alt="truc" />
+            </div>
+            {isOpen && <div className='collapse_content'>
+                {children}
+            </div>}
+        </div>
+        
+    )
+};
+
+export default Collapse;
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,9 +1,28 @@
 import Header from '../../components/header/header.jsx'
+import Banner from '../../components/banner/banner.jsx'
+import bannerAbout from '../../assets/bannerAbout.png'
+import Footer from '../../components/footer/footer.jsx'
+import Collapse from '../../components/collapse/collapse.jsx'
 
 
 
 function About () {
-    return <nav><Header /></nav>
+    return (
+        <div>
+            <Header />
+            <Banner imageUrl={ bannerAbout} />
+            <Collapse title='test 1'>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum ullam corporis, quibusdam sapiente eos officia tempore optio rem exercitationem placeat ipsum minima mollitia, necessitatibus vel incidunt quo, consectetur repudiandae. Doloremque.</p>
+            </Collapse>
+            <Collapse title='test 2'>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum ullam corporis, quibusdam sapiente eos officia tempore optio rem exercitationem placeat ipsum minima mollitia, necessitatibus vel incidunt quo, consectetur repudiandae. Doloremque.</p>
+            </Collapse>
+            <Collapse title='test 3'>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum ullam corporis, quibusdam sapiente eos officia tempore optio rem exercitationem placeat ipsum minima mollitia, necessitatibus vel incidunt quo, consectetur repudiandae. Doloremque.</p>
+            </Collapse>
+            <Footer />
+        </div>
+    )
 }
 
 export default About;
